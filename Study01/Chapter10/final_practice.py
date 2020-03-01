5238eba80ca35cedfe5f2aa3a0518712e0cb47ba
Song <songyj63@gmail.com>
2020-03-01 21:23:58 +0900

    from local

--- Study01/Chapter10/final_practice.py
+++ Study01/Chapter10/final_practice.py
@@ -1,12 +1,5 @@
 #%%
+import tensorflow as tf
 import numpy as np
 
-data = np.loadtxt('C:/Users/VUNO/Desktop/Programming/Git/Study/Study01/Chapter10/data/housing.data.txt')
-
-X = data[:,0:13]
-Y = data[:,13]
-
-print(Y)
-print(X[0,:])
-
-# %%
+print(6)
