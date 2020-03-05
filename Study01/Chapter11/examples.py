5e143cc4463c3c9238562fdc134471afa4c806fc
Song <songyj63@gmail.com>
2020-03-05 23:03:11 +0900

    cc

---
+++ Study01/Chapter11/examples.py
@@ -0,0 +1,37 @@
+#%%
+# leakyReLU, selu
+import tensorflow as tf
+from tensorflow import keras
+
+model = keras.models.Sequential([
+    keras.layers.Dens(10, kernel_initializer='he_normal'),
+    keras.layers.LeakyReLU(alpha=0.2),
+
+    keras.layers.Dense(10, activation='selu', kernel_initializer='lecun_nrmal')
+])
+
+
+#%%
+# batch norm eaxmple
+
+model = keras.models.Sequential([
+    keras.layers.Flatten(input_shape=[28, 28]),
+    keras.layers.BatchNormalization(),
+    keras.layers.Dense(300, activation="elu", kernel_initializer="he_normal"),
+    keras.layers.BatchNormalization(),
+    keras.layers.Dense(100, activation="elu", kernel_initializer="he_normal"),
+    keras.layers.BatchNormalization(),
+    keras.layers.Dense(10, activation="softmax")
+])
+
+model = keras.models.Sequential([
+    keras.layers.Flatten(input_shape=[28, 28]),
+    keras.layers.BatchNormalization(),
+    keras.layers.Dense(300, kernel_initializer="he_normal", use_bias=False),
+    keras.layers.BatchNormalization(),
+    keras.layers.Activation("elu"),
+    keras.layers.Dense(100, kernel_initializer="he_normal", use_bias=False),
+    keras.layers.BatchNormalization(),
+    keras.layers.Activation("elu"),
+    keras.layers.Dense(10, activation="softmax")
+])
