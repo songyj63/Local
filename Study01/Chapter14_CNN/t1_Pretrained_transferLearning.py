570c8bb860158991d24fc865099a0b25b7c2cb50
Song <songyj63@gmail.com>
2020-03-29 14:31:52 +0900

    cc

--- Study01/Chapter14_CNN/t1_Pretrained_transferLearning.py
+++ Study01/Chapter14_CNN/t1_Pretrained_transferLearning.py
@@ -57,3 +57,12 @@ history = model.fit(train_set,
                     epochs=40)
     
 #%%
+# localization
+base_model = keras.applications.xception.Xception(weights="imagenet", include_top=False)
+avg = keras.layers.GlobalAveragePooling2D()(base_model.output)
+class_output = keras.layers.Dense(n_classes, activation="softmax")(avg)
+loc_output = keras.layers.Dense(4)(avg)
+model = keras.Model(inputs=base_model.input, outputs=[class_output, loc_output])
+model.compile(loss=["sparse_categorical_crossentropy", "mse"],
+                    loss_weights=[0.8, 0.2], # depends on what you care most about
+                    optimizer=optimizer, metrics=["accuracy"])
