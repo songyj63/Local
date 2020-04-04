da3fca4d0ebb82e46298b7900f8c18add1e25756
Song <songyj63@gmail.com>
2020-04-04 18:22:43 +0900

    cc

---
+++ Study01/Chapter15_ProcessingSequence_RNN_CNN/examples.py
@@ -0,0 +1,134 @@
+#%%
+# forcasting a time series
+
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+
+def generate_time_series(batch_size, n_steps):
+    freq1, freq2, offsets1, offsets2 = np.random.rand(4, batch_size, 1)
+    time = np.linspace(0, 1, n_steps)
+    series = 0.5 * np.sin((time - offsets1) * (freq1 * 10 + 10)) # wave 1
+    series += 0.2 * np.sin((time - offsets2) * (freq2 * 20 + 20)) # + wave 2
+    series += 0.1 * (np.random.rand(batch_size, n_steps) - 0.5) # + noise
+    return series[..., np.newaxis].astype(np.float32)
+
+
+n_steps = 50
+series = generate_time_series(10000, n_steps + 1)
+X_train, y_train = series[:7000, :n_steps], series[:7000, -1]
+X_valid, y_valid = series[7000:9000, :n_steps], series[7000:9000, -1]
+X_test, y_test = series[9000:, :n_steps], series[9000:, -1]
+
+
+def plot_series(series, y=None, y_pred=None, x_label="$t$", y_label="$x(t)$"):
+    plt.plot(series, ".-")
+    if y is not None:
+        plt.plot(n_steps, y, "bx", markersize=10)
+    if y_pred is not None:
+        plt.plot(n_steps, y_pred, "ro")
+    plt.grid(True)
+    if x_label:
+        plt.xlabel(x_label, fontsize=16)
+    if y_label:
+        plt.ylabel(y_label, fontsize=16, rotation=0)
+    plt.hlines(0, 0, 100, linewidth=1)
+    plt.axis([0, n_steps + 1, -1, 1])
+
+
+# compare last bit and y
+y_pred = X_valid[:, -1]
+print(np.mean(keras.losses.mean_squared_error(y_valid, y_pred)))
+
+plot_series(X_valid[0, :, 0], y_valid[0, 0], y_pred[0, 0])
+plt.show()
+
+#%%
+
+# implementing a simple RNN
+model = keras.models.Sequential([
+    keras.layers.SimpleRNN(1, input_shape=[None, 1])
+])
+
+optimizer = keras.optimizers.Adam(lr=0.005)
+model.compile(loss="mse", optimizer=optimizer)
+history = model.fit(X_train, y_train, epochs=20,
+                    validation_data=(X_valid, y_valid))
+
+model.evaluate(X_valid, y_valid)
+
+y_pred = model.predict(X_valid)
+plot_series(X_valid[0, :, 0], y_valid[0, 0], y_pred[0, 0])
+plt.show()
+
+
+
+#%%
+# deep RNNs
+
+model = keras.models.Sequential([
+    keras.layers.SimpleRNN(20, return_sequences=True, input_shape=[None, 1]),
+    keras.layers.SimpleRNN(20),
+    keras.layers.Dense(1)
+])
+
+model.compile(loss="mse", optimizer="adam")
+history = model.fit(X_train, y_train, epochs=20,
+                    validation_data=(X_valid, y_valid))
+
+model.evaluate(X_valid, y_valid)
+
+y_pred = model.predict(X_valid)
+plot_series(X_valid[0, :, 0], y_valid[0, 0], y_pred[0, 0])
+plt.show()
+
+
+#%%
+# forecasting several time steps ahead
+
+n_steps = 50
+series = generate_time_series(10000, n_steps + 10)
+X_train = series[:7000, :n_steps]
+X_valid = series[7000:9000, :n_steps]
+X_test = series[9000:, :n_steps]
+Y = np.empty((10000, n_steps, 10))
+for step_ahead in range(1, 10 + 1):
+    Y[..., step_ahead - 1] = series[..., step_ahead:step_ahead + n_steps, 0]
+Y_train = Y[:7000]
+Y_valid = Y[7000:9000]
+Y_test = Y[9000:]
+
+print(X_train.shape)
+print(Y_train.shape)
+
+def plot_multiple_forecasts(X, Y, Y_pred):
+    n_steps = X.shape[1]
+    ahead = Y.shape[1]
+    plot_series(X[0, :, 0])
+    plt.plot(np.arange(n_steps, n_steps + ahead), Y[0, :, 0], "ro-", label="Actual")
+    plt.plot(np.arange(n_steps, n_steps + ahead), Y_pred[0, :, 0], "bx-", label="Forecast", markersize=10)
+    plt.axis([0, n_steps + ahead, -1, 1])
+    plt.legend(fontsize=14)
+
+model = keras.models.Sequential([
+    keras.layers.SimpleRNN(20, return_sequences=True, input_shape=[None, 1]),
+    keras.layers.SimpleRNN(20, return_sequences=True),
+    keras.layers.TimeDistributed(keras.layers.Dense(10))
+])
+
+def last_time_step_mse(Y_true, Y_pred):
+    return keras.metrics.mean_squared_error(Y_true[:, -1], Y_pred[:, -1])
+
+model.compile(loss="mse", optimizer=keras.optimizers.Adam(lr=0.01), metrics=[last_time_step_mse])
+history = model.fit(X_train, Y_train, epochs=20,
+                    validation_data=(X_valid, Y_valid))
+
+series = generate_time_series(1, 50 + 10)
+X_new, Y_new = series[:, :50, :], series[:, 50:, :]
+Y_pred = model.predict(X_new)[:, -1][..., np.newaxis]
+
+plot_multiple_forecasts(X_new, Y_new, Y_pred)
+plt.show()
